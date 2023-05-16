ff77f96822ae4d5129df7ed9a149bed2843ddd6f
kadiiravsaar <kadiiravsar@gmail.com>
2023-05-16 18:08:51 +0300

    CodeFirst

---
+++ Code_First_Ders_8/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ECommerceDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-G34LNC7;Database=ECommerce;Trusted_Connection=true;TrustServerCertificate=True;");
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+}
+
+public class Product
+{
+    public int Id { get; set; } 
+    public string Name { get; set; }
+
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+
+}
