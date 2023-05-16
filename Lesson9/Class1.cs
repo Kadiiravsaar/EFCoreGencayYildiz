1a9c87b25c45e616246702006375863831c939e4
kadiiravsaar <kadiiravsar@gmail.com>
2023-05-16 18:26:39 +0300

    .

---
+++ Lesson9/Class1.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+public class ETicaret : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-G34LNC7;Database=ETicaret;Trusted_Connection=true;TrustServerCertificate=True;");
+
+
+        public DbSet<Urun> Uruns { get; set; }
+}
+}
+
+
+public class Urun
+{
+    public int Id { get; set; }
+    public string UrunAdi { get; set; }
+    public decimal UrunFiyati { get; set; }
+
+}
