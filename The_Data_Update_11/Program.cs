e4e4a46db0191884d6d1efd6d20dd71de47c8ed5
kadiiravsaar <kadiiravsar@gmail.com>
2023-05-19 21:19:02 +0300

    ...

---
+++ The_Data_Update_11/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using The_Data_Update_11;
+
+//ETicaretContext context = new ETicaretContext();
+
+//Urun urun = await context.Urunler.FirstOrDefaultAsync(x => x.Id == 1);
+//urun.Name = "EFcoreGencay";
+
+//var urun = await context.Urunler.FirstOrDefaultAsync(x => x.Id == 1);
+//urun.Name = "EFcoreGencay";   bunlar da olur fakat farkı bir nesneye aktarmak
+
+//await context.SaveChangesAsync();
+
+
+#region  ChangeTracker nedir
+// ChangeTracker context üzerinden gelen verilerin takibinden sorumlu bir mekanizmadır.
+// bu takip mekanızması sayesinde context üzerinden gelen verilerle ilgili işlemler neticesinde günc veya silme sorgularının oluşturacağı anlaşılır.
+
+#endregion
+
+#region Takip edilmeyen nesneler nasıl güncellenir
+//ETicaretContext context = new ETicaretContext();
+
+//Urun Urun = new Urun()
+//{
+//    Id=3,
+//    Name="keke"
+//};
+
+//context.Urunler.Update(Urun);
+//await context.SaveChangesAsync();
+
+#endregion
+
+#region EntityState
+// bir entity instancesinin durumunu ifade eder:
+
+//ETicaretContext context = new ETicaretContext();
+
+//Urun urun = new Urun();
+//Console.WriteLine(context.Entry(urun).State);
+
+#endregion
---
+++ The_Data_Update_11/Urun.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Data_Update_11
+{
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ETicaretContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-G34LNC7;Database=ETicaret;Trusted_Connection=true;TrustServerCertificate=True;");
+
+        }
+        public DbSet<Urun> Urunler { get; set; }
+    }
+}
