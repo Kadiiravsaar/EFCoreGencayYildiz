c3b0348f0dcb642eb2daf821e1dbcdd175265b1f
Kadiiravsaar <kadiiravsar@gmail.com>
2023-06-03 11:42:13 +0300

    where,orderby

--- Querying_13_2/Program.cs
+++ Querying_13_2/Program.cs
@@ -40,13 +40,32 @@ ETicarettContext context = new ETicarettContext();
 
 #region 14. Ders Çoğul veri getiren sorgular
 
+#region Where
+//var urunler = context.Urunler.Where(u => u.UrunAdi.EndsWith("6")).ToList();
 
-//var urunler = context.Urunler.Where(u => u.Id>500).ToList();
 
 
-var urunler = from urun in context.Urunler
-           where urun.Id > 50 && urun.UrunAdi.EndsWith("6")
-           select urun;
-var data = await urunler.ToListAsync();
-Console.WriteLine();
+//var urunler = from urun in context.Urunler
+//           where urun.Id > 50 && urun.UrunAdi.EndsWith("6")
+//           select urun;
+//var data = await urunler.ToListAsync();
+//Console.WriteLine();
+#endregion
+
+#region OrderBy
+//var urunler = context.Urunler.Where(u => u.Id<15).OrderByDescending(u => u.UrunAdi).ToList(); sadece orderBY(cccc) yazarsak asc dir
+
+//var urunller1 = (from urun in context.Urunler
+//                where urun.Id<15
+//                orderby urun.UrunAdi
+//                select urun).ToList();
+//Console.WriteLine();
+
+// ThenBy => order by da yapılan işlemleri farklı kolonlarda da yapmamızı sağlar
+
+
+var urunler = context.Urunler.Where(u => u.Id<15).OrderBy(u => u.UrunAdi).ThenBy(u=>u.Fiyat).ToList(); 
+// eğer urunadında aynı şeyler varsa fiyata göre de sırala demiş olduk
+
+#endregion
 #endregion
