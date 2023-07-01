0121f1ef1e29a12c88bec80b38e65dc35e596b77
Kadiiravsaar <kadiiravsar@gmail.com>
2023-07-01 17:36:12 +0300

    Ders 21 - 1

---
+++ Relationshpis/Program.cs
@@ -0,0 +1,37 @@
+﻿// Çalışanlar ve departmanlar adında iki tablo var
+
+// Çalışanlar                          Departmanlar     
+//    Id                                      Id
+//    Adi                                     DepartmanAdi        
+//    DepartmanId
+
+#region Principal entity (Asıl entity)
+// kendi başına varolabilen, tek başına ,bağımsız tabloya karşılık gelir
+// Departman burda asıl entitydir çünkü departmanlar çalışan olmadan da vardır 
+
+
+#endregion
+
+
+
+#region Dependent entity (Bağımlı entity)
+
+
+
+
+#endregion
+
+
+#region Foreign Key
+
+
+
+
+#endregion
+
+#region Primary Key
+
+
+
+
+#endregion
