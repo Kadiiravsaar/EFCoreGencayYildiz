22a1732cde054407eda9f4a58a139812cbaf0758
Kadiiravsaar <kadiiravsar@gmail.com>
2023-07-03 17:07:56 +0300

    revize

--- Relationshpis/Program.cs
+++ Relationshpis/Program.cs
@@ -66,16 +66,50 @@ class Departman
 
 #endregion
 
-#region İlişki Türleri
 #region One to One
-
+//Çalışan ile adresi arasındaki ilişki,
+//Karı koca arasındaki ilişki.
 #endregion
 
 #region One to Many
-
+//Çalışan ile departman arasındaki ilişki,
+//Anne ve çocukları arasındaki ilişki.
 #endregion
 
 #region Many to Many
+//Çalışanlar ile projeler arasındaki ilişki,
+//Kardeşler arasındaki ilişki.
+#endregion
 
+#region Entity Framework Core'da İlişki Yapılandırma Yöntemleri
+#region Default Conventions
+//Varsayılan entity kurallarını kullanarak yapılan ilişki yapılandırma yöntemleridir.
+
+//Navigation property'leri kullanarak ilişki şablonlarını çıkarmaktadır.
+#endregion
+
+#region Data Annotations Attributes
+//Entity'nin niteliklerine göre ince ayarlar yapmamızı sağlayan attribute'lardır. [Key], [ForeignKey]
+#endregion
+
+#region Fluent API
+
+//Entity modellerindeki ilişkileri yapılandırırken daha detaylı çalışmamızı sağlayan yöntemdir.
+
+#region HasOne
+//İlgili entity'nin ilişkisel entity'ye birebir ya da bire çok olacak şekilde ilişkisini yapılandırmaya başlayan metottur.
+#endregion
+
+#region HasMany
+//İlgili entity'nin ilişkisel entity'ye çoka bir ya da çoka çok olacak şekilde ilişkisini yapıulandırmaya başlayan metottur.
+#endregion
+
+#region WithOne
+//HasOne ya da HasMany'den sonra bire bir ya da çoka bir olacak şekilde ilişki yapılandırmasını tamamlayan metottur.
+#endregion
+
+#region WithMany
+//HasOne ya da HasMany'den sonra bire çok ya da çoka çok olacak şekilde ilişki yapılandırmasını tamamlayan metottur.
+#endregion
 #endregion
 #endregion
