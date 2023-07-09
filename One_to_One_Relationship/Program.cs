36375f125b6412fe032501ffa7886f6e7ea85aa9
Kadiiravsaar <kadiiravsar@gmail.com>
2023-07-09 17:18:27 +0300

    ONE_TO_ONE

---
+++ One_to_One_Relationship/Program.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#region Default Convention
+//class Calisan
+//{
+//    public int Id { get; set; }
+//    public string CalisanAdi { get; set; }
+
+//    public CalisanAdresi CalisanAdresi { get; set; }
+//}
+
+//class CalisanAdresi
+//{
+//    public int Id { get; set; }
+//    public int CalisanId { get; set; } // eğer biz bunu yazmazsak ef core hangisainin dependent entitiy olduğunu anlamaz.
+//    public string Adres { get; set; }
+
+//    public Calisan Calisan { get; set; }
+//}
+
+
+#endregion
+
+#region Data Annotations
+
+//class Calisan
+//{
+//    public int Id { get; set; }
+//    public string CalisanAdi { get; set; }
+
+//    public CalisanAdresi CalisanAdresi { get; set; }
+//}
+
+//class CalisanAdresi
+//{
+//    public int Id { get; set; } // primary
+
+//    [ForeignKey(nameof(Calisan))] bu da olur
+
+//    public int CalisanId { get; set; }  // foregin 
+
+//    public string Adres { get; set; }
+
+//    public Calisan Calisan { get; set; }
+//}
+
+
+#endregion
+
+
+#region Fluent Api
+
+class Calisan
+{
+    public int Id { get; set; }
+    public string CalisanAdi { get; set; }
+
+    public CalisanAdresi CalisanAdresi { get; set; }
+}
+
+class CalisanAdresi
+{
+    public int Id { get; set; }
+    public string Adres { get; set; }
+
+    public Calisan Calisan { get; set; }
+}
+
+
+#endregion
+
+
+class ESirketContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-G34LNC7;Database=ESirketDb;Trusted_Connection=true;TrustServerCertificate=True;");
+    }
+    public DbSet<CalisanAdresi> CalisanAdresis { get; set; }
+    public DbSet<Calisan> Calisans { get; set; }
+
+
+    // modellerin (entitylerin) veritabanında konfigure edilmesini sağlar 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    { // fluent api denir
+
+        modelBuilder.Entity<CalisanAdresi>()
+            .HasKey(c => c.Id);  // CalisanAdresi içerisindeki ıd primary key özelliği ezilmemesi için 
+
+        modelBuilder.Entity<Calisan>()
+             .HasOne(c => c.CalisanAdresi)
+            .WithOne(c => c.Calisan)
+            .HasForeignKey<CalisanAdresi>(c => c.Calisan);
+    }
+}
+
+
