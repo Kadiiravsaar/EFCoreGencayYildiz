362338a4cfe2d351a3d285a3239fb7d62d0dc54d
Kadiiravsaar <kadiiravsar@gmail.com>
2023-07-19 16:05:15 +0300

    One to Many  relationship added

---
+++ One_to_Many_Relationship/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+#region Default Convention
+
+// bire çok ilişkide foreign key tanımlama zorunluluğu yokturrrr
+
+#endregion
+
+class ESirketMany : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-G34LNC7;Database=ESirketManyDb;Trusted_Connection=true;TrustServerCertificate=True;");
+
+
+    }
+    public DbSet<Calisan> Calisanlar { get; set; }
+    public DbSet<Departman> Departmanlar { get; set; }
+
+}
+
+class Calisan // dependent entity Çünkü departmana bağlı
+{
+    public int Id { get; set; }
+    public string Ad { get; set; }
+
+    // ******* public int DepartmanId { get; set; } // ******* bire çok ilişkide foreign key tanımlama zorunluluğu yokturrrr **** 
+    
+    public Departman Departman { get; set; } // navigation  prop
+} 
+
+
+class Departman
+{
+    public int Id { get; set; }
+    public string DepartmanAd { get; set; }
+
+    public ICollection<Calisan> Calisanlar { get; set; } // navigation  prop
+}
+
