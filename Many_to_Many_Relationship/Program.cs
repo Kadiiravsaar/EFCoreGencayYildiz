67b6c0fa34e33a7d12b3f15df74358e7bf879ba8
Kadiiravsaar <kadiiravsar@gmail.com>
2023-07-22 18:21:55 +0300

    Many to many added

--- Many_to_Many_Relationship/Program.cs
+++ Many_to_Many_Relationship/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+#region Default Convention
+
+
+#endregion
+
+class ESirketMany : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-G34LNC7;Database=ESirketmanytomanyDb;Trusted_Connection=true;TrustServerCertificate=True;");
+
+
+    }
+    public DbSet<Kitap> Kitaplar { get; set; }
+    public DbSet<Yazar> Yazarlar { get; set; }
+
+}
+
+class Kitap 
+{
+    public int Id { get; set; }
+    public string KitapAd { get; set; }
+    public ICollection<Yazar> Yazarlar { get; set; } // navigation  prop
+
+}
+
+
+class Yazar
+{
+    public int Id { get; set; }
+    public string YazarAdi { get; set; }
+    public ICollection<Kitap> Kitaplar { get; set; } // navigation  prop
+}
+
